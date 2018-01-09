d99153d413c4591affc900f43e36696435fc93ed
Jean Lange <langeje@us.ibm.com>
2018-01-09 08:11:16 -0500

    Initial Commit

---
+++ Elf on a Shelf/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Elf on a Shelf
+//
+//  Created by Jean Lange on 1/9/18.
+//  Copyright © 2018 Jean Lange. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Elf on a ShelfTests/Elf_on_a_ShelfTests.swift
@@ -0,0 +1,36 @@
+//
+//  Elf_on_a_ShelfTests.swift
+//  Elf on a ShelfTests
+//
+//  Created by Jean Lange on 1/9/18.
+//  Copyright © 2018 Jean Lange. All rights reserved.
+//
+
+import XCTest
+@testable import Elf_on_a_Shelf
+
+class Elf_on_a_ShelfTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
