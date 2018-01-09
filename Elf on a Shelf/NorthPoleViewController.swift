63846117f17df945f1d68939d4be388225d87866
Jean Lange <langeje@us.ibm.com>
2018-01-09 11:06:38 -0500

    add sceneview to northpole

--- Elf on a Shelf/NorthPoleViewController.swift
+++ Elf on a Shelf/NorthPoleViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2018 Jean Lange. All rights reserved.
 //
 
+import ARKit
 import UIKit
 
 class NorthPoleViewController: UIViewController {
@@ -18,13 +19,21 @@ class NorthPoleViewController: UIViewController {
     }
 
     @IBOutlet weak var weatherInfoLabel: UILabel!
+    @IBOutlet weak var sceneView: ARSCNView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         weatherInfoLabel.text = "Hey, it worked!"
         fetchTemperature()
+
+        let configuration = ARWorldTrackingConfiguration()
+        sceneView.session.run(configuration)
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
 }
 
 extension NorthPoleViewController: TemperatureFetching {}
