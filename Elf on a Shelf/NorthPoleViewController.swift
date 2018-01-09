1eeb1201eb31aa02cd2f432d31a2a07c0efec5ba
Jean Lange <langeje@us.ibm.com>
2018-01-09 08:49:07 -0500

    labels, viewcontroller, request

---
+++ Elf on a Shelf/NorthPoleViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NorthPoleViewController.swift
+//  Elf on a Shelf
+//
+//  Created by Jean Lange on 1/9/18.
+//  Copyright © 2018 Jean Lange. All rights reserved.
+//
+
+import UIKit
+
+class NorthPoleViewController: UIViewController {
+
+    @IBOutlet weak var weatherInfoLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        guard let url = URL(string: "https://api.wunderground.com/api/b193c8afeeecdbb2/conditions/q/AK/North_Pole.json") else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let session = URLSession.shared
+
+        session.dataTask(with: request) { data, response, error in
+            if error != nil {
+                print("😡😡😡😡😡\(error.debugDescription)")
+            } else {
+                print("🤩🤩🤩🤩🤩 Made It!\(String(describing: response))")
+            }
+            }.resume()
+
+        weatherInfoLabel.text = "Hey, it worked!"
+    }
+
+}
